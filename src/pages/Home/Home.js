787ca8e4e6f4928c62955d84aa82b9dedf58736d
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-12-22 11:38:49 -0700

    initializing components Card, Search, Title

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from "react";
+import "./style.css";
+//import Outside from "../../components/Outside";
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                {/* <Outside /> */}
+            </div>
+        )
+    }
+}
+export default Home;
