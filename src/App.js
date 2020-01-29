55f586d60efc1a79af3a60ad3ed484ba85fea231
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2020-01-29 11:08:41 -0700

    overpassed coors issue by adding cors proxy prior to each api url

--- src/App.js
+++ src/App.js
@@ -51,46 +51,66 @@ class App extends React.Component {
     e.preventDefault(); //prevents default behavior of component when we press button else refreshes entire page
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; //preventing no-cors policy issue
 
+    /*---------------------------------API Call 1---------------------------------*/
+    const url1 = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`;
     //imperial units = Farenheit where metric = Celcius
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial` /* ,
-      {
-        mode: "no-cors",
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-      } */
-    );
-    //Declaing API data object
-    const data = await api_call.json();
+    const api_call = await fetch(proxyurl + url1, {
+      method: "GET",
 
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ":",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Authorization"
+      }
+    });
+    //Declaring API data object
+    const data = await api_call.json();
+    console.log(data);
+    /*---------------------------------API Call 2---------------------------------*/
+
+    const url2 = `http://dataservice.accuweather.com/locations/v1/${country}/search?apikey=${API_KEY2}&q=${city}`;
     //API fetching the code for city to use when pulling hourly data
-    const api_call2 = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/${country}/search?apikey=${API_KEY2}&q=${city}` /* ,
-      {
-        mode: "no-cors",
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-      } */
-    );
+    const api_call2 = await fetch(proxyurl + url2, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Authorization"
+      }
+    });
     //Declaing API data object
     const data2 = await api_call2.json();
+    console.log(data2);
+    //Getting City Key for hourly weather
     const cityKey = data2[0].Key;
-    console.log(cityKey);
 
-    const api_call3 = await fetch(
-      `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityKey}?apikey=${API_KEY2}` /* ,
-      {
-        mode: "no-cors",
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-      } */
-    );
+    /*---------------------------------API Call 3---------------------------------*/
+    const url3 = `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityKey}?apikey=${API_KEY2}`;
+    const api_call3 = await fetch(proxyurl + url3, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": ":",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Authorization"
+      }
+    });
     //Declaing API data object
     const data3 = await api_call3.json();
 
     console.log(data3);
 
+    /*---------------------------------Setting State to grab data needed---------------------------------*/
     this.setState({
       allResults: [
         {
